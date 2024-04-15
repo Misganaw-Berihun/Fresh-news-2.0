c67cd13c8930e08562da3271f0572ebc5a2b6a09
Misganaw-Berihun <misganawbmb@gmail.com>
2024-04-15 16:47:56 +0300

    dependency setup

--- tasks.py
+++ tasks.py
@@ -1,10 +1,11 @@
 from robocorp.tasks import task
-from robocorp.tasks import get_output_dir
 from RPA.Browser.Selenium import Selenium
-from selenium.webdriver.common.keys import Keys
+from RPA.Excel.Files import Files
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-@task
-def minimal_task():
-    browser = Selenium(auto_close=False)
-    browser.open_available_browser('https://www.latimes.com/')
-    browser.click_element_when_visible(xpath="/html/body/ps-header/header/div[2]/button")
+from enum import Enum
+from typing import List
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import re
